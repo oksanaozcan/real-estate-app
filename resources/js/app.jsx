b45ca6240cf7bea39f029b4005f397621f4cae8e
oksanaozcan <mekansa83@gmail.com>
2025-03-11 14:04:33 +0300

    breez react auth scaffolding added

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,14 +1,25 @@
-import './bootstrap';
 import '../css/app.css';
+import './bootstrap';
 
-import React from 'react'
-import {createRoot} from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/react';
-import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`,import.meta.glob('./Pages/**/*.jsx')),
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob('./Pages/**/*.jsx'),
+        ),
     setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />)
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
     },
-})
+    progress: {
+        color: '#4B5563',
+    },
+});
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,33 @@
 <?php
 
-use Inertia\Inertia;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/', function () {
-    return Inertia::render('Home', [
-        'user' => auth()->user(),
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
 });
 
-Route::middleware([AdminMiddleware::class, 'auth:sanctum'])->group(function () {
-    Route::get('/admin', function () {
-        return 'Hello Admin';
-    });
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/admin', function () {
+    return Inertia::render('Admin/Test');
+})->middleware(['auth', 'verified', AdminMiddleware::class])->name('test');
+
+
+require __DIR__.'/auth.php';
