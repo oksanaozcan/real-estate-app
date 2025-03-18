0795d58ed54000196b853e2a5fc71e2d866b7ff6
oksanaozcan <mekansa83@gmail.com>
2025-03-18 11:09:51 +0300

    added translations for ru and tr

---
+++ lang/tr/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sayfalandırma Dil Satırları
+    |--------------------------------------------------------------------------
+    |
+    | Aşağıdaki dil satırları, basit sayfalandırma bağlantılarını oluşturmak
+    | için paginator kütüphanesi tarafından kullanılır. Uygulamanıza daha iyi
+    | uyum sağlaması için bunları istediğiniz gibi değiştirebilirsiniz.
+    |
+    */
+
+    'previous' => '&laquo; Önceki',
+    'next' => 'Sonraki &raquo;',
+
+];
---
+++ lang/tr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Şifre Sıfırlama Dil Satırları
+    |--------------------------------------------------------------------------
+    |
+    | Aşağıdaki dil satırları, şifre sıfırlama girişimi sonucunda
+    | ortaya çıkabilecek durumlar için varsayılan mesajları içerir.
+    | Örneğin, geçersiz bir token veya başarısız güncelleme gibi.
+    |
+    */
+
+    'reset' => 'Şifreniz sıfırlandı.',
+    'sent' => 'Şifre sıfırlama bağlantısını e-posta ile gönderdik.',
+    'throttled' => 'Lütfen tekrar denemeden önce bekleyin.',
+    'token' => 'Bu şifre sıfırlama jetonu geçersiz.',
+    'user' => "Bu e-posta adresine sahip bir kullanıcı bulunamadı.",
+
+];
